fab7d1b85fe0b3ad4d53757b84e957d52a524844
paraz <mail2paras.s@gmail.com>
2018-10-17 22:52:20 +0530

    minor changes

--- json_to_csv.py
+++ json_to_csv.py
@@ -2,17 +2,24 @@ import sys
 import json
 import csv
 
-tw_header = ['id', 'screen_name', 'place', 'country_code', 'user_location', 'name', 'text', 'created_at', 'is_quoted', "source",
+
+# set json file path
+json_file_path = "cleaned_twitter_elections_data.json"
+
+tw_header = ['id', 'screen_name', 'place', 'country_code', 'user_location', 'name', 'text', 'favorite_count', 'retweet_count',  'created_at', 'is_quoted', "source",
              'q_id', 'q_screen_name', 'q_name', 'q_text', 'q_created_at', 'q_source']
 
-csv_writer_t = csv.writer(open("status.csv", "w"))
+# csv file name
+csv_writer_t = csv.writer(open("status_new.csv", "w"))
 
 csv_writer_t.writerow(tw_header)
 
 
-data = json.load(open(sys.argv[1]))
-# print(json.dumps(data[0], indent=4))
+data = json.load(open(json_file_path))
+
 data_t = {}
+
+total_tweets = 0
 for t in data:
     for i in tw_header:
         data_t[i] = None
@@ -21,6 +28,8 @@ for t in data:
         data_t['screen_name'] = t['user']['screen_name']
         data_t['name'] = t['user']['name']
         data_t['text'] = t['text']
+        data_t['favorite_count'] = t['favorite_count']
+        data_t['retweet_count'] = t['retweet_count']
         data_t['created_at'] = t['created_at']
         data_t['is_quoted'] = t['is_quote_status']
         data_t['source'] = t['source']
@@ -44,3 +53,7 @@ for t in data:
         print("Inconsistency: " + str(e))
         print("Inconsistency at {}".format(t['id']))
     csv_writer_t.writerow(data_t.values())
+    total_tweets+=1
+    
+print("\nSaved csv with {} rows".format(total_tweets))
+
